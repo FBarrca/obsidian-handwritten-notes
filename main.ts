b8e3da5db17dc89397c940153195402ff671b836
unknown <201800222@alu.comillas.edu>
2023-08-01 16:22:10 +0200

    First commit

--- main.ts
+++ main.ts
@@ -1,124 +1,219 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	Setting,
+	normalizePath,
+	FileSystemAdapter,
+	DataAdapter,
+	Platform,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+export function buildPluginStaticResourceSrc(plug: NotePDF, assetPath: string) {
+	return plug.app.vault.adapter.getResourcePath(
+		plug.app.vault.configDir +
+			"/plugins/" +
+			plug.manifest.id +
+			"/" +
+			assetPath
+	);
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export class PDFCreatorModal extends Modal {
+	result: { name: string; template: string } = {
+		name: "New note",
+		template: "blank.pdf",
+	};
+	onSubmit: (result: { name: string; template: string }) => void;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
+	constructor(
+		app: App,
+		onSubmit: (result: { name: string; template: string }) => void
+	) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		let { contentEl } = this;
+
+		contentEl.createEl("h1", { text: "Create New note" });
+		new Setting(contentEl).setName("Name").addText((text) => {
+			text.setValue(this.result["name"]);
+			text.onChange((value) => {
+				this.result["name"] = value;
+			});
+		});
+		new Setting(contentEl).setName("Template").addDropdown((dropDown) => {
+			dropDown.addOption("blank.pdf", "blank.pdf");
+			dropDown.addOption("lined.pdf", "lined.pdf");
+			dropDown.onChange(async (value) => {
+				this.result["template"] = value;
+			});
+		});
+		new Setting(contentEl).addButton((btn) =>
+			btn
+				.setButtonText("Submit")
+				.setCta()
+				.onClick(() => {
+					this.close();
+					this.onSubmit(this.result);
+				})
+		);
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		let { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export default class NotePDF extends Plugin {
+	async loadPdfTemplate(template: string) {
+		// check if it is a mobile adapter
+		let result = null;
+		console.log("Adapter: breaks here");
+		if (this.app.adapter instanceof FileSystemAdapter) {
+			console.log("Desktop");
+			console.log();
+			result = await this.app.vault.adapter.readBinary(
+				this.app.vault.configDir +
+					"/plugins/obsidian-sample-plugin/" +
+					template
+			);
+		} else {
+			console.log("Mobile");
+			// its a DataAdapter
+			console.log("Name: " + template);
+			result = await this.app.vault.adapter.readBinary(
+				normalizePath(
+					this.app.vault.configDir +
+						"/plugins/obsidian-sample-plugin/" +
+						template
+				)
+			);
+		}
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+		return result;
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	async createPDF() {
+		await new PDFCreatorModal(this.app, async (result) => {
+			const pathToPlugin = normalizePath(
+				this.app.vault.configDir + "/plugins/obsidian-sample-plugin/"
+			);
+			const pathToPdf = normalizePath(
+				pathToPlugin + "/" + result.template
+			);
+			console.log("Path to PDF: " + pathToPdf);
+			const useRoot = false;
+			const destFolder = useRoot
+				? this.app.vault.getRoot()
+				: this.app.workspace.getActiveFile()?.parent;
 
-		containerEl.empty();
+			if (destFolder) {
+				const template = await this.loadPdfTemplate(result.template);
+				console.log("Template: " + template);
+				const path = destFolder.path + "/" + result.name + ".pdf";
+				console.log("Creating PDF at " + path);
+				await this.app.vault.createBinary(path, template);
+			}
+		}).open();
+	}
+
+	async onload() {
+		const ribbonIconEl = this.addRibbonIcon(
+			"dice",
+			"Create empty PDF note",
+			(evt: MouseEvent) => {
+				this.createPDF();
+			}
+		);
+
+		this.addCommand({
+			id: "create-empty-pdf-note",
+			name: "Create empty PDF note",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.createPDF();
+			},
+		});
+		const ribbonIconEl2 = this.addRibbonIcon(
+			"pencil",
+			"Create empty PDF note",
+			(evt: MouseEvent) => {
+				this.app.commands.executeCommandById(
+					"open-with-default-app:open"
+				);
+				// console.log( this.app.commands.app.commands.commands);
+				// Write in a new markdown file
+				// this.app.vault.create("New note.md", this.app.commands.app.commands.commands);
+				// write the json as a string to a file
+				const commands = this.app.commands.app.commands.commands;
+				// make it so we can map over it
+
+				console.log(
+					JSON.stringify(this.app.commands.app.commands.commands)
+				);
+				this.app.vault.create(
+					"New note.md",
+					JSON.stringify(this.app.commands.app.commands.commands)
+				);
+			}
+		);
+		this.app.workspace.on("active-leaf-change", () => {
+			console.log("Active leaf changed!");
+			// get current leaf
+			const leaf = this.app.workspace.activeLeaf;
+			// get current view
+			const view = leaf.view;
+			// console.log(view);
+			const activeFile = this.app.workspace.getActiveFile();
+			if (activeFile?.extension === "pdf") {
+				console.log("PDF file opened!");
+				//  find HTML element with class 'markdown-preview-sizer'
+				// get Document
+				const doc = view.containerEl.ownerDocument;
+				console.log(doc);
+				const toolbars =
+					document.getElementsByClassName("pdf-toolbar");
+				//  Loop through all the elements with class 'markdown-preview-sizer'
+				for (let i = 0; i < toolbars.length; i++) {
+					//  check if it has a child with id 'annotate'
+					if (
+						!toolbars[i].contains(
+							document.getElementById("annotate"+i)
+						)
+					) {
+						// add a child inside markdownPreviewSizer
+						const button = doc.createElement("button");
+						// give it an id of 'annotate' + i
+						button.id = "annotate"+i;
+						button.innerHTML = "Annotate";
+						button.onclick = () => {
+							this.app.commands.executeCommandById(
+								"open-with-default-app:open"
+							);
+						};
+						toolbars[i].appendChild(button);
+					}
+				};
+				// else if md check if there are embedded pdfs
+			}
+			//  else if (activeFile?.extension === "md") {
+			// 	const embededPdfs = document.getElementsByClassName("pdf-embed");
+			// 	// Loop through all the elements with class 'pdf-embed' foreach does not work
+			// 	for (let i = 0; i < embededPdfs.length; i++) {
+			// 		// get its child pdf-toolbar
+			// 		const toolbar = embededPdfs[i].getElementsByClassName(
+			// 			"pdf-toolbar"
+			// 		)[0];
+			// 		if (!toolbar) continue;
+			// 		if (embededPdfs[i].contains(
+			// 			document.getElementById("annotate"+i)
+			// 		)) continue;
 
 		new Setting(containerEl)
 			.setName('Setting #1')
@@ -127,8 +222,12 @@ class SampleSettingTab extends PluginSettingTab {
 				.setPlaceholder('Enter your secret')
 				.setValue(this.plugin.settings.mySetting)
 				.onChange(async (value) => {
+					console.log('Secret: ' + value);
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
 	}
+	onLayoutReady() {}
+
+	onunload() {}
 }
