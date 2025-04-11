1fc99098b65118cfd3a07481e2a882f2b9813010
Mara <mara-li@outlook.fr>
2025-04-11 16:33:40 +0200

    refactor: update ChooseDestinationSearch to handle string paths instead of TFolder

--- src/dialogs/PathModals.ts
+++ src/dialogs/PathModals.ts
@@ -24,37 +24,37 @@ export class ChooseDestModals extends SuggestModal<TFolder> {
 		el.createEl("small", { text: value.path });
 	}
 
-	onChooseSuggestion(item: TFolder, evt: MouseEvent | KeyboardEvent) {
+	onChooseSuggestion(item: TFolder, _evt: MouseEvent | KeyboardEvent) {
 		this.onSubmit(item);
 		this.close();
 	}
 }
 
-export class ChooseDestinationSearch extends AbstractInputSuggest<TFolder> {
+export class ChooseDestinationSearch extends AbstractInputSuggest<string> {
 	constructor(
 		private inputEl: HTMLInputElement,
 		app: App,
-		private onSubmit: (value: TFolder) => void,
+		private onSubmit: (value: string) => void,
 	) {
 		super(app, inputEl);
 	}
 
-	renderSuggestion(value: TFolder, el: HTMLElement) {
-		el.setText(value.path);
+	renderSuggestion(value: string, el: HTMLElement) {
+		el.setText(value);
 	}
 
-	protected getSuggestions(query: string): TFolder[] {
-		return (
-			this.app.vault
-				//@ts-ignore
-				.getAllFolders()
-				.filter((folder: TFolder) => {
-					return folder.path.toLowerCase().contains(query.toLowerCase());
-				})
-		);
+	protected getSuggestions(query: string): string[] {
+		const sugg = this.app.vault
+			.getAllFolders()
+			.filter((folder: TFolder) => {
+				return folder.path.toLowerCase().contains(query.toLowerCase());
+			})
+			.map((folder: TFolder) => folder.path);
+		if (sugg.length === 0) return [query];
+		return sugg;
 	}
-	selectSuggestion(value: TFolder, _evt: MouseEvent | KeyboardEvent): void {
-		this.inputEl.value = value.path;
+	selectSuggestion(value: string, _evt: MouseEvent | KeyboardEvent): void {
+		this.inputEl.value = value;
 		this.onSubmit(value);
 		this.inputEl.focus();
 		this.inputEl.trigger("input");
